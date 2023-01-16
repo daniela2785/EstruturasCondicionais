752d47d7c22d56a7c89283a700dbb793f1d73a85
daniela marques <dd978452@gmail.com>
2023-01-16 09:38:06 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturasCondicionais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EstruturasCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 2;
+
+            if (10 > 20) // SE
+            {
+                Console.WriteLine("É verdade");
+            }
+            else if (20 >= 20)
+            {
+                Console.WriteLine("Executou Elseif");
+            }
+            else //SE NÃO
+            {
+                Console.WriteLine("É mentira");
+            }
+
+        }
+    }
+}
